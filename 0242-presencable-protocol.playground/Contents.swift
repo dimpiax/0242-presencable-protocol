91ee2fe4cb4c86bbe24cd79b867e50e952b1ab11
Dmytro Pylypenko <dimpiax@gmail.com>
2019-01-29 22:19:21 +0100

    initial commit

---
+++ 0242-presencable-protocol.playground/Contents.swift
@@ -0,0 +1,45 @@
+/*:
+ ## Proposal for Swift Evolution
+ # Presencable Protocol
+ [0242-presencable-protocol](https://github.com/dimpiax/swift-evolution/blob/master/proposals/0242-presencable-protocol.md)
+ */
+import UIKit
+
+protocol Emptiable {
+    var isEmpty: Bool { get }
+}
+
+protocol Presencable: Emptiable {
+    var presence: Self? { get }
+}
+
+extension Presencable {
+    var presence: Self? {
+        return isEmpty ? nil : self
+    }
+}
+
+// example with Array and ArraySlice types only
+extension Array: Presencable {
+}
+
+extension ArraySlice: Presencable {
+}
+
+let arr = [1, 3]
+arr.dropFirst(2).presence // nil
+arr.presence // [1, 3]
+
+// custom type
+struct Foo: Presencable {
+    let collection: Set<Int>
+
+    var isEmpty: Bool { return collection.isEmpty }
+
+    init(_ values: Int...) {
+        collection = Set(values)
+    }
+}
+
+Foo(1, 2, 3).presence // Foo
+Foo().presence // nil
---
+++ README.md
@@ -0,0 +1,3 @@
+## Proposal for Swift Evolution
+# Presencable Protocol
+[0242-presencable-protocol](https://github.com/dimpiax/swift-evolution/blob/master/proposals/0242-presencable-protocol.md)
